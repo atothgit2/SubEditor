7ccb03c930fd51f834e2510fa85652097f3fb013
atothgit <toth.arpad@fluenta.eu>
2021-09-05 23:17:44 +0200

    sub delay class created, timing modifier service test file added

---
+++ src/main/java/com/arpi/subeditor/parser/SubDisplayDurationModifierService.java
@@ -0,0 +1,24 @@
+package com.arpi.subeditor.parser;
+
+import com.arpi.subeditor.parser.SubEntry;
+import com.arpi.subeditor.model.Pair;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import java.util.List;
+
+public class SubDisplayDurationModifierService {
+//    public List<SubEntry> modifySubEntriesDisplayDuration(List<SubEntry> entriesFromParser, double durationModifierInSecs) {
+//        for (SubEntry entry : entriesFromParser) {
+//            String timingLine = entry.timing;
+//            Pair<String> timestamps = new Pair<String>();
+//            timestamps = getTimestampsAsStrings(timingLine);
+//            String adjustedTimestamp1 = adjustTime(timestamps.getFirst(), modifierInSecs);
+//            String adjustedTimestamp2 = adjustTime(timestamps.getSecond(), modifierInSecs);
+//            //              a fenti itterátort is szervízbe vagy util-ba tenni?
+//            //             az adjustTime-ot szervizbe kitenni és itt meghívni
+//        }
+//        return entriesFromParser;
+//    }
+}
---
+++ src/test/java/com/arpi/subeditor/timingmodifierservice/TestTimingModifierService.java
@@ -0,0 +1,32 @@
+package com.arpi.subeditor.timingmodifierservice;
+
+import com.arpi.subeditor.parser.SubEntry;
+import com.arpi.subeditor.parser.TimingModifierService;
+import org.junit.Assert;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+public class TestTimingModifierService {
+    @Test
+    public void shouldModifyTiming() {
+        SubEntry subEntry = new SubEntry();
+        subEntry.setIndex("4");
+        subEntry.setTiming("00:00:27,861 --> 00:00:31,657");
+        subEntry.setTextConcat("- Ahogy nálunk szokás." + "\r\n" + "- Őrültek vagytok.");
+
+        List<SubEntry> subEntries = new ArrayList<SubEntry>();
+        subEntries.add(subEntry);
+
+        double addTimeInSecs = 3661;
+
+        TimingModifierService timingModifierService = new TimingModifierService();
+        List<SubEntry> results = timingModifierService.modifySubEntriesTiming(subEntries, addTimeInSecs);
+
+        Assert.assertEquals(1, subEntries.size());
+        Assert.assertEquals("4", results.get(0).index);
+        Assert.assertEquals("01:01:28,860 --> 01:01:32,657", results.get(0).timing);
+        Assert.assertEquals("- Ahogy nálunk szokás." + "\r\n" + "- Őrültek vagytok." + "\r\n", results.get(0).text);
+        }
+    }
